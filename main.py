03d91b5f52dfd409d7dd70a0e624644f51945809
udaykommaraju <udaypocgcp@gmail.com>
2025-08-28 16:41:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Cloud Run!!!! test"
+    return "Hello from Cloud Run!!!! testing"
 
 if __name__ == "__main__":
     app.run()
